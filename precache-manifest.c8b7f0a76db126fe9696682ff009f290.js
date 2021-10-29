b87cdce601ef224bb5c2fc4ab1b79760768d9c22
Darshan Raval <darshanraval3546@gmail.com>
2021-10-29 23:48:41 +0530

    Updates

---
+++ precache-manifest.c8b7f0a76db126fe9696682ff009f290.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40774a0a06cd115695c5fbd26c59d8f4",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "fd16120c597bf472a6f9",
+    "url": "/weather-app/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "4bf1c37496ccab5730c3",
+    "url": "/weather-app/static/css/main.7b484e31.chunk.css"
+  },
+  {
+    "revision": "fd16120c597bf472a6f9",
+    "url": "/weather-app/static/js/2.8acb1a8e.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/weather-app/static/js/2.8acb1a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf1c37496ccab5730c3",
+    "url": "/weather-app/static/js/main.2de049b2.chunk.js"
+  },
+  {
+    "revision": "37c738ef312e86ba523e",
+    "url": "/weather-app/static/js/runtime-main.32279052.js"
+  }
+]);
